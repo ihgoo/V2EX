f4a8490f73d02c9777a85724568be8ec86309df0
ihgoo <ihgoo2008@gmail.com>
2015-05-19 16:39:45 +0800

    init BaseApplication

---
+++ app/src/main/java/me/xunhou/v2ex/app/BaseApplication.java
@@ -0,0 +1,57 @@
+package me.xunhou.v2ex.app;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.util.SparseArray;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by ihgoo on 2015/1/19.
+ */
+public class BaseApplication extends Application {
+
+    static Context instance;
+
+    @Override
+    public void onCreate() {
+        instance = getApplicationContext();
+        Fresco.initialize(this);
+        super.onCreate();
+    }
+
+
+
+    public static WeakReference<Activity> instanceRef;
+
+    public static synchronized Context getInstance() {
+        if (instanceRef == null || instanceRef.get() == null) {
+            return BaseApplication.getContext();
+        } else {
+            return instanceRef.get();
+        }
+    }
+
+    public static synchronized Activity getActivity() {
+        Activity result = null;
+        if (instanceRef != null && instanceRef.get() != null) {
+            result = instanceRef.get();
+        }
+        return result;
+    }
+
+    public static synchronized Context getContext() {
+        return instance;
+    }
+
+    public static SparseArray<WeakReference<Activity>> taskStack = new SparseArray<WeakReference<Activity>>();
+
+    public static synchronized SparseArray<WeakReference<Activity>> getTaskStack() {
+        return taskStack;
+    }
+
+
+}
