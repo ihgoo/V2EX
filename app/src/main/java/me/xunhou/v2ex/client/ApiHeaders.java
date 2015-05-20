7902ab7ef52f920ac2cb6d73e918ba8b242ffe9e
ihgoo <ihgoo2008@gmail.com>
2015-05-20 17:12:28 +0800

    add header

--- app/src/main/java/me/xunhou/v2ex/client/ApiHeaders.java
+++ app/src/main/java/me/xunhou/v2ex/client/ApiHeaders.java
@@ -17,6 +17,8 @@ public class ApiHeaders implements RequestInterceptor {
     }
 
     @Override public void intercept(RequestFacade request) {
+        request.addHeader("User-Agent", "Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19");
+        request.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
         if (sessionId != null) {
 //            request.setHeader(...);
         }
