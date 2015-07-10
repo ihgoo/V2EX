6c81220a23b76dd723818753df5d75b05404bb8a
ihgoo <ihgoo2008@gmail.com>
2015-07-10 14:14:59 +0800

    修改http的拦截器，让其自动处理cookie

--- app/src/main/java/me/xunhou/v2ex/client/ApiHeaders.java
+++ app/src/main/java/me/xunhou/v2ex/client/ApiHeaders.java
@@ -1,30 +1,16 @@
 package me.xunhou.v2ex.client;
 
-import android.text.TextUtils;
-
-import me.xunhou.v2ex.model.V2EXSettingHelper;
 import retrofit.RequestInterceptor;
 
 /**
  * Created by ihgoo on 2015/5/19.
  */
 public class ApiHeaders implements RequestInterceptor {
-    private String sessionId;
 
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-
-    public void clearSessionId() {
-        sessionId = null;
-    }
-
-    @Override public void intercept(RequestFacade request) {
-        request.addHeader("User-Agent", "Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19");
-        request.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-        if (!TextUtils.isEmpty( V2EXSettingHelper.getInstance().getA2())) {
-            request.addHeader("set-cookie", V2EXSettingHelper.getInstance().getSession());
-            request.addHeader("set-cookie", V2EXSettingHelper.getInstance().getA2());
-        }
+    @Override
+    public void intercept(RequestFacade request) {
+        request.addHeader("User-Agent", "Mozilla/5.0 (Linux; U; Android 4.1.1; zh-cn; HTC One X - 4.1.1 - API 16 - 720x1280 Build/JRO03S) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30");
+        request.addHeader("Accept", "*/*");
+        request.addHeader("Accept-Language", "zh-cn,zh");
     }
 }
